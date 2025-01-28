28b6a6609bc914625419202142aa9a3c54364a65
yazdan <yazdaner8@gmail.com>
2025-01-28 19:42:31 +0330

    wallet 1.0.0

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-const INITIAL_DIFFICULTY = 4;
+const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000;
+const STARTING_BALANCE = 1000;
 
 const GENESIS_DATA = {
   timestamp: 1,
-  lastHash: '------',
-  hash: 'hash-one',
+  lastHash: "------",
+  hash: "hash-one",
   difficulty: INITIAL_DIFFICULTY,
   nonce: 0,
-  data: []
-}
+  data: [],
+};
 
-module.exports = {GENESIS_DATA,MINE_RATE};
+module.exports = { GENESIS_DATA, MINE_RATE, STARTING_BALANCE };
