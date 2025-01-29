0b7d4e8340274a1337d75209c186e08d025d60fd
yazdan <yazdaner8@gmail.com>
2025-01-29 21:38:53 +0330

    wallet 1.1.0

--- wallet/index.js
+++ wallet/index.js
@@ -1,12 +1,17 @@
 const {STARTING_BALANCE} = require('./../config')
 const {ec} = require('./../util');
+const cryptoHash = require('./../util/crypto-hash');
 
-const keyPair = ec.genKeyPair();
 
 class Wallet{
   constructor(){
     this.balance = STARTING_BALANCE;
-    this.publicKey = keyPair.getPublic().encode('hex');
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
+  }
+
+  sign(data){
+    return this.keyPair.sign(cryptoHash(data));
   }
 }
 
