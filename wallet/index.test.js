247c5c9cc950e53adb7b431c97a67d0ab075ba2a
yazdan <yazdaner8@gmail.com>
2025-01-30 20:23:49 +0330

    Transaction 1.3.0

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require("./index");
 const { verifySignature } = require("./../util");
+const Transaction = require("./transaction");
 
 describe("Wallet", () => {
   let wallet;
@@ -15,7 +16,6 @@ describe("Wallet", () => {
   it("has a `publicKey`", () => {
     expect(wallet).toHaveProperty("publicKey");
   });
-  
   describe("signing data", () => {
     const data = "some-data";
     it("verifies a signature", () => {
@@ -35,6 +35,37 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data),
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction()", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws an error", () => {
+        expect(() =>
+          wallet.createTransaction({
+            amount: 999999,
+            recipient: "foo-recipient",
+          })
+        ).toThrow("amount exceeds balance");
+      });
+    });
+    describe("and the amount is valid", () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = "foo-recipeint";
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+      it("creates an instance of `Transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+      it("outputs the amount the recipient", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
