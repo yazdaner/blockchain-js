cbe4019f2872cc7d64c4f1a117086bcd295ec7fa
yazdan <yazdaner8@gmail.com>
2025-01-31 13:05:06 +0330

    transaction pool 1.1.0

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,23 +1,35 @@
-const TrasactionPool = require("./transaction-pool");
+const TransactionPool = require("./transaction-pool");
 const Transaction = require("./transaction");
 const Wallet = require("./index");
 
 describe("TransactionPool", () => {
-  let transactionPool, trasaction;
+  let transactionPool, transaction, senderWallet;
 
   beforeEach(() => {
-    transactionPool = new TrasactionPool();
-    trasaction = new Transaction({
-      senderWallet: new Wallet(),
+    senderWallet = new Wallet();
+    transactionPool = new TransactionPool();
+    transaction = new Transaction({
+      senderWallet: senderWallet,
       recipient: "test-recipient",
       amount: 50,
     });
   });
 
   describe("setTransaction()", () => {
-    it("adds a trasaction", () => {
-      transactionPool.setTransaction(trasaction);
-      expect(transactionPool.transactionMap[trasaction.id]).toBe(trasaction);
+    it("adds a transaction", () => {
+      transactionPool.setTransaction(transaction);
+      expect(transactionPool.transactionMap[transaction.id]).toBe(transaction);
+    });
+  });
+
+  describe("existingTransaction()", () => {
+    it("returns an existing transaction given an input address", () => {
+      transactionPool.setTransaction(transaction);
+      expect(
+        transactionPool.existingTransaction({
+          inputAddress: senderWallet.publicKey,
+        })
+      ).toBe(transaction);
     });
   });
 });
