01a92ffee6a780311699183a0052a226d8cd933e
yazdan <yazdaner8@gmail.com>
2025-02-01 16:45:49 +0330

    miner 1.0.0

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -32,4 +32,40 @@ describe("TransactionPool", () => {
       ).toBe(transaction);
     });
   });
+
+  describe("validTransactions()", () => {
+    let validTrnsactions, errorMock;
+
+    beforeEach(() => {
+      validTrnsactions = [];
+      errorMock = jest.fn();
+      global.console.error = errorMock;
+      for (let i = 0; i < 10; i++) {
+        transaction = new Transaction({
+          senderWallet,
+          recipient: "any-recipient",
+          amount: 30,
+        });
+
+        if (i % 3 === 0) {
+          transaction.input.amount = 99999;
+        } else if (i % 3 === 1) {
+          transaction.input.signature = new Wallet().sign("foo");
+        } else {
+          validTrnsactions.push(transaction);
+        }
+
+        transactionPool.setTransaction(transaction);
+      }
+    });
+
+    it("returns valid transactions", () => {
+      expect(transactionPool.validTransactions()).toEqual(validTrnsactions);
+    });
+
+    it("logs errors for the invalid transactions", () => {
+      transactionPool.validTransactions();
+      expect(errorMock).toHaveBeenCalled();
+    });
+  });
 });
