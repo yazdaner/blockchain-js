7115f1ee060cd112d2ae66eb4059ce6559b69c55
yazdan <yazdaner8@gmail.com>
2025-02-01 18:35:28 +0330

    miner 1.3.0

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,17 +1,25 @@
+const Transaction = require("./../wallet/transaction");
+
 class TransactionMiner {
-  constructor({ blockchian, transactionPool, wallet, pubsub }) {
-    this.blockchian = blockchian;
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
     this.transactionPool = transactionPool;
     this.wallet = wallet;
     this.pubsub = pubsub;
   }
 
   mineTransactions() {
-    // get the transactino pool's valid transactions
-    // generate the miner's reward
-    // add a block consisting of these transactions to blockchain
-    // broadcast the updated blockchain
-    // clear the pool
+    const validTransactions = this.transactionPool.validTransactions();
+
+    validTransactions.push(
+      Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
+
+    this.blockchain.addBlock({ data: validTransactions });
+
+    this.pubsub.broadcastChain();
+
+    this.transactionPool.clear();
   }
 }
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Blockchain = require("./blockchain");
 const Pubsub = require("./app/pubsub");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
+const TransactionMiner = require("./app/transaction-miner");
 const tcpPortUsed = require("tcp-port-used");
 const axios = require("axios");
 
@@ -14,8 +15,20 @@ const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new Pubsub({ blockchain, transactionPool });
 
+const transactionMiner = new TransactionMiner({
+  blockchain,
+  transactionPool,
+  wallet,
+  pubsub,
+});
+
 app.get("/api/blocks", (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.get("/api/mine-transactions", (req, res) => {
+  transactionMiner.mineTransactions();
+  res.redirect("/api/blocks");
 });
 
 app.get("/api/transaction-pool-map", (req, res) => {
@@ -39,7 +52,6 @@ app.post("/api/transact", (req, res) => {
   }
   transactionPool.setTransaction(transaction);
   pubsub.broadcastTransaction(transaction);
-
   res.json({ transaction });
 });
 app.post("/api/mine", (req, res) => {
