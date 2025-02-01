9827647291c2de9716783032c3b95eff3a52e76b
yazdan <yazdaner8@gmail.com>
2025-02-01 17:06:50 +0330

    miner 1.2.0

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -25,6 +25,21 @@ class TransactionPool {
       Transaction.validTransaction(transaction)
     );
   }
+
+  clear() {
+    this.transactionMap = {};
+  }
+
+  clearBlockchainTransactions({ chain }) {
+    for (let i = 0; i < chain.length; i++) {
+      const block = chain[i];
+      for (let transaction of block.data) {
+        if (this.transactionMap[transaction.id]) {
+          delete this.transactionMap[transaction.id];
+        }
+      }
+    }
+  }
 }
 
 module.exports = TransactionPool;
